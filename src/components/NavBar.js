04c538362a268b3dab73a581816738ef86c7423e
Brandon Tsui <itstsui@gmail.com>
2020-06-18 11:12:05 -0400

    navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,51 @@ import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import { Link } from "react-router-dom";
 
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 const NavBar = () => {
-    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const classes = useStyles();
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-    return (
-        <div>
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <div>
             {!isAuthenticated && (
-                <button onClick={() => loginWithRedirect({})}>Log in</button>
+              <button onClick={() => loginWithRedirect({})}>Log in</button>
             )}
 
-            {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
             {isAuthenticated && (
-                <span>
-                    <Link to="/">Home</Link>&nbsp;
-                    <Link to="/profile">Profile</Link>
-                </span>
+              <button onClick={() => logout()}>Log out</button>
             )}
-        </div>
-    );
+            {isAuthenticated && (
+              <span>
+                <Link to="/">Home</Link>&nbsp;
+                <Link to="/profile">Profile</Link>
+              </span>
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default NavBar;
